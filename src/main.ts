6d3a695854d5bb8b040c54ef11d13d4851c5e9a4
knky-ru <2718868@mail.ru>
2021-12-19 19:53:57 +0300

    Remove konva

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
-import VueKonva from 'vue-konva';
 
 import Ionic from '@ionic/vue';
 import '@ionic/core/css/ionic.bundle.css';
 
-Vue.use(VueKonva);
 Vue.use(Ionic);
 Vue.config.productionTip = false;
 
